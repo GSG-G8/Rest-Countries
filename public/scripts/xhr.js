064159a5a9c52a5d17a9a5ae9f5a28e727e27581
MohammedIrheem <mohammedirheem96@gmail.com>
2020-02-11 16:19:56 +0200

    create xhr function relates #7

---
+++ public/scripts/xhr.js
@@ -0,0 +1,11 @@
+
+module.exports = (url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+};
