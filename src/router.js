7a581f3960335829480e8f1b26c36682135e97b1
Malek.Zd <muhammadamin@pop-os.localdomain>
2020-02-12 11:06:39 +0200

    Add the static handlers Relates #13

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 const homeHandler = require('./handlers/homeHandler');
+const staticHandler = require('./handlers/staticHandler');
+
 
 const router = (req, res) => {
   const endpoint = req.url;
   if (endpoint === '/') {
     homeHandler(req, res);
+  } else if (endpoint.includes('public')) {
+    staticHandler(req, res);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
     res.end('<h1> Error 404: Not Found! </h1>');
