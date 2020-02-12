24db7ae5b45b9c57e25321f2514900bacbea9e81
MohammedIrheem <mohammedirheem96@gmail.com>
2020-02-12 09:52:49 +0200

    handle errors relates #7

--- public/scripts/xhr.js
+++ public/scripts/xhr.js
@@ -2,8 +2,13 @@
 module.exports = (url, callback) => {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(JSON.parse(xhr.responseText));
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(JSON.parse(xhr.responseText));
+      } else {
+        // eslint-disable-next-line no-console
+        console.log(xhr.status);
+      }
     }
   };
   xhr.open('GET', url);
