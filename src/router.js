a5a84cec54f37c794b35135743de027d7acc4303
Malek.Zd <muhammadamin@pop-os.localdomain>
2020-02-12 12:55:22 +0200

    Add indexHandler and modify xhr.js file Relates #23

--- src/router.js
+++ src/router.js
@@ -1,13 +1,12 @@
-const homeHandler = require('./handlers/homeHandler');
-const staticHandler = require('./handlers/staticHandler');
+const indexHandler = require('./handlers/index');
 
 
 const router = (req, res) => {
   const endpoint = req.url;
   if (endpoint === '/') {
-    homeHandler(req, res);
+    indexHandler.homeHandler(req, res);
   } else if (endpoint.includes('public')) {
-    staticHandler(req, res);
+    indexHandler.staticHandler(req, res);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
     res.end('<h1> Error 404: Not Found! </h1>');
