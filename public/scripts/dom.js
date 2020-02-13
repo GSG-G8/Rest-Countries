c73f57e67dbd374f401f7a2780795e73a3fb3162
Malek.Zd <muhammadamin@pop-os.localdomain>
2020-02-13 09:32:47 +0200

    Re-Factor the dom function and remove action from form Reltes #43

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -6,8 +6,6 @@ const countriesNameUrl = '/countries';
 let countriesInfoUrl;
 
 
-
-
 const insertedCountry = (e) => {
   apiFunction(countriesNameUrl, (res) => {
     while (countryList.firstChild) countryList.removeChild(countryList.firstChild);
@@ -27,66 +25,34 @@ const insertedCountry = (e) => {
   });
 };
 
+const showDetails = (name, apiValue) => {
+  const container = document.createElement('div');
+  const detailName = document.createElement('span');
+  const detailValue = document.createElement('span');
+  container.appendChild(detailName);
+  container.appendChild(detailValue);
+  detailName.textContent = `${name}: `;
+  detailValue.textContent += apiValue;
+  container.setAttribute('class', 'name');
+  countryInfoContainer.appendChild(container);
+};
+
 const countryInfo = () => {
   apiFunction(countriesInfoUrl, (res) => {
-    while (countryInfoContainer.firstChild) countryInfoContainer
-      .removeChild(countryInfoContainer.firstChild);
+    while (countryInfoContainer.firstChild) {
+      countryInfoContainer
+        .removeChild(countryInfoContainer.firstChild);
+    }
     const countryFlag = document.createElement('img');
     countryFlag.src = res[0].flag;
     countryFlag.setAttribute('class', 'flag');
-
-    const nameContainer = document.createElement('div');
-    const name = document.createElement('span');
-    const countryName = document.createElement('span');
-    nameContainer.appendChild(name);
-    nameContainer.appendChild(countryName);
-    name.textContent = 'Country Name: ';
-    nameContainer.textContent += res[0].name;
-    nameContainer.setAttribute('class', 'country-name');
-
-    const capitalContainer = document.createElement('div');
-    const capital = document.createElement('span');
-    const CountryCapital = document.createElement('span');
-    capitalContainer.appendChild(capital);
-    capitalContainer.appendChild(CountryCapital);
-    capital.textContent = 'Capital: ';
-    capitalContainer.textContent += res[0].capital;
-    capitalContainer.setAttribute('class', 'capital');
-
-    const currencyContainer = document.createElement('div');
-    const currency = document.createElement('span');
-    const CountryCurrency = document.createElement('span');
-    currencyContainer.appendChild(currency);
-    currencyContainer.appendChild(CountryCurrency);
-    currency.textContent = 'Currency: ';
-    CountryCurrency.textContent += res[0].currencies[0].name;
-    currencyContainer.setAttribute('class', 'currency');
-
-    const languageContainer = document.createElement('div');
-    const language = document.createElement('span');
-    const CountryLanguage = document.createElement('span');
-    languageContainer.appendChild(language);
-    languageContainer.appendChild(CountryLanguage);
-    language.textContent = 'Language: ';
-    CountryLanguage.textContent += res[0].languages[0].name;
-    languageContainer.setAttribute('class', 'language');
-
-
-    const populationContainer = document.createElement('div');
-    const population = document.createElement('span');
-    const CountryPopulation = document.createElement('span');
-    populationContainer.appendChild(population, CountryPopulation);
-    populationContainer.appendChild(CountryPopulation);
-    population.textContent = 'Population: ';
-    CountryPopulation.textContent += res[0].population;
-    populationContainer.setAttribute('class', 'population');
-
     countryInfoContainer.appendChild(countryFlag);
-    countryInfoContainer.appendChild(nameContainer);
-    countryInfoContainer.appendChild(capitalContainer);
-    countryInfoContainer.appendChild(currencyContainer);
-    countryInfoContainer.appendChild(languageContainer);
-    countryInfoContainer.appendChild(populationContainer);
+
+    showDetails('Country-Name', res[0].name);
+    showDetails('capital', res[0].capital);
+    showDetails('Currency', res[0].currencies[0].name);
+    showDetails('Language', res[0].languages[0].name);
+    showDetails('Population', res[0].population);
   });
 };
 
